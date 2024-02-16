65745e4974f1f703de8b15cb653d95d91ecbcace
Sam21sop <sbussiness21@gmail.com>
2024-02-16 12:22:17 +0530

    component added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HomePage from './component/HomePage/HomePage.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Learn React</h1>
-    </div>
+      <div className="App">
+        <h1>Learn React</h1>
+        <HomePage/>
+      </div>
   );
 }
 
---
+++ src/component/Form/Form.js
@@ -0,0 +1,25 @@
+import { email, name } from "../HomePage/HomePage.js";
+
+function handleSubmit(e) {
+  e.preventDefault();
+}
+
+const Form = () => (
+  <>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h3>Login Page</h3>
+        <input type="text" placeholder="Name" value={name} />
+        <br />
+        <br />
+        <input type="email" placeholder="Email" value={email} />
+        <br />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </>
+);
+
+
+export default Form;
