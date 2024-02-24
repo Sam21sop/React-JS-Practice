2e4a627bb832f049078a39aa72e3afead747f612
Sam21sop <sbussiness21@gmail.com>
2024-02-24 22:00:19 +0530

    button variant added

---
+++ ButtonVariants/src/ButtonVariants.styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+// Complete the below given ButtonView style Component
+
+export const ButtonView = styled.button`
+/* Default styles */
+  background-color: #fff;
+  color: #000;
+  border-color: #000;
+  
+  /* Conditional styles based on props */
+  ${(props) =>
+    props.filled &&
+    `
+      background-color: ${props.bg || 'inherit'};
+      color: ${props.color || 'inherit'};
+      border: none;
+    `
+  }
+`;
