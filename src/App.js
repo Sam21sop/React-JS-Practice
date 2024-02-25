162f745963ab589eac0fd92c9877f91fe25bb5b4
Sam21sop <sbussiness21@gmail.com>
2024-02-25 18:54:53 +0530

    initial upload

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import HomePage from './component/HomePage/HomePage.js';
 function App() {
   return (
       <div className="App">
-        <h1>Learn React</h1>
         <HomePage/>
       </div>
   );
